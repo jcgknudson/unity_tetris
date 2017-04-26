ca9d13c6512de85b2c012b87e7d27dd6b71ad1b5
= <jboehrer@wisc.edu>
2017-04-26 12:14:56 -0500

    initialize "player gameobject" within script

--- tetris/Assets/_Scripts/GameManager.cs
+++ tetris/Assets/_Scripts/GameManager.cs
@@ -4,27 +4,30 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
-    public float TickDelay;
+    public float tick_delay;
+    public GameObject player;
     private float time_of_last_tick;
-    public List<ITickable> Tickables;
+    public List<ITickable> tickables;
 	
 	void Start () {
-        Tickables = new List<ITickable>();
+        tickables = new List<ITickable>();
         time_of_last_tick = Time.time;
+        player = new GameObject();
+        player.AddComponent<Player>();
     }
 	
 	// Update is called once per frame
 	void Update () {
         float elapsed_time = Time.time - time_of_last_tick;
 
-        if (elapsed_time > TickDelay) {
+        if (elapsed_time > tick_delay) {
             time_of_last_tick = Time.time;
             Tick();
         }
 	}
 
     void Tick() {
-        foreach (ITickable tickable in Tickables) {
+        foreach (ITickable tickable in tickables) {
             tickable.Tick();
         }
 
